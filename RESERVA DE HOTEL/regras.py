44954c8deae68d4f8465d5fe9ba669d081f5d53d
valexi25 <106202030+valexi25@users.noreply.github.com>
2024-10-20 08:39:34 -0300

    praticando as variables
    praticando os tipos de variables

--- RESERVA DE HOTEL/regras.py
+++
@@ -1,9 +0,0 @@
-#Criar um sistema de reserva de hotel que contenga as seguintes informação 
-#nome do hospe
-#Dias de reservas
-#tarifa diaria 
-#indicar se tem visita ao mar 
-#despois imprimir os valores de cada variavle
-
-
-  
