06ac7a6483cdf2d290e59079ba9cdd26f1f288ba
valexi25 <106202030+valexi25@users.noreply.github.com>
2024-10-20 08:29:51 -0300

    exemplos de practicas
    praticando novamente

---
+++ RESERVA DE HOTEL/regras.py
@@ -0,0 +1,9 @@
+#Criar um sistema de reserva de hotel que contenga as seguintes informação 
+#nome do hospe
+#Dias de reservas
+#tarifa diaria 
+#indicar se tem visita ao mar 
+#despois imprimir os valores de cada variavle
+
+
+  
